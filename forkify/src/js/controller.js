d7bce35da92c71c745dc39ac130f5aa2a98da740
EliezerAmbion <eliezerambion@gmail.com>
2021-04-18 13:51:18 +0800

    added forkify

---
+++ forkify/src/js/controller.js
@@ -0,0 +1,54 @@
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
+
+import 'core-js/stable'; //polyfilling everything else
+import 'regenerator-runtime/runtime'; //polyfilling async await
+import { async } from 'regenerator-runtime';
+
+if (module.hot) {
+  module.hot.accept();
+}
+
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+
+    if (!id) return;
+    recipeView.renderSpinner();
+
+    // 1) Loading recipe
+    await model.loadRecipe(id);
+
+    // 2) Rendering recipe
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    recipeView.renderError();
+  }
+};
+
+const controlSearchResults = async function () {
+  try {
+    resultsView.renderSpinner();
+
+    // 1) Get search query
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    // 2) Load search results
+    await model.loadSearchResults(query);
+
+    //3) Render results
+    console.log(model.state.search.results);
+    resultsView.render(model.state.search.results);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
+};
+init();
---
+++ forkify/src/js/views/searchView.js
@@ -0,0 +1,22 @@
+class SearchView {
+  _parentElement = document.querySelector('.search');
+
+  getQuery() {
+    const query = this._parentElement.querySelector('.search__field').value;
+    this._clearInput();
+    return query;
+  }
+
+  _clearInput() {
+    this._parentElement.querySelector('.search__field').value = '';
+  }
+
+  addHandlerSearch(handler) {
+    this._parentElement.addEventListener('submit', function (e) {
+      e.preventDefault(); // prevent reload of page when submit is clicked
+      handler();
+    });
+  }
+}
+
+export default new SearchView();
