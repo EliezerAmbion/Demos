e735fc258fe5edcac93083211658b7319e958489
EliezerAmbion <eliezerambion@gmail.com>
2021-04-29 14:49:58 +0800

    remove forkify

--- forkify/src/js/config.js
+++
@@ -1,5 +0,0 @@
-export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
-export const TIMEOUT_SEC = 10;
-export const RES_PER_PAGE = 10;
-export const KEY = 'dc4a01f5-79c5-479d-bc31-c98b569d0425';
-export const MODAL_CLOSE_SEC = 2.5;
